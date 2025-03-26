7c28f209a22a97804d9bb851ff68f508c72284f3
Ramon Ott <ramon@julith.ch>
2025-03-26 11:53:45 +0100

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 use find::find_dirs;
 use serde::Deserialize;
+use skim::prelude::Event;
 use skim::prelude::SkimItemReader;
 use skim::prelude::SkimOptionsBuilder;
 use skim::Skim;
+use skim::SkimOutput;
 use std::env;
 use std::fs;
 use std::io::Cursor;
 use std::io::{self, Write};
 use std::path::Path;
+use std::process;
 use std::process::{Command, Stdio};
 
 mod find;
 
 #[derive(Debug, Deserialize)]
 struct Config {
-    sessions: Vec<SSHSession>,
+    sessions: Vec<Session>,
     directory: Vec<Directory>,
 }
 
@@ -26,11 +29,11 @@ struct Directory {
 }
 
 #[derive(Debug, Deserialize)]
-struct SSHSession {
+struct Session {
     name: String,
     protocol: String,
-    host: String,           // e.g. "root@host"
-    remote_command: String, // e.g. "cd /var/www; nvim ."
+    host: Option<String>, // e.g. "root@host"
+    command: String,      // e.g. "cd /var/www; nvim ."
     split: Option<SplitConfig>,
 }
 
@@ -98,14 +101,19 @@ fn main() {
         let input = combined.join("\n");
         let options = SkimOptionsBuilder::default()
             .header(Some("RUSTY SESSIONIZER".to_string()))
-            .reverse(true)
+            .reverse(false)
             .build()
             .unwrap();
         let item_reader = SkimItemReader::default();
         let items = item_reader.of_bufread(Cursor::new(input));
 
         let mut selected_item = String::new();
+
         if let Some(output) = Skim::run_with(&options, Some(items)) {
+            if output.final_event == Event::EvActAbort {
+                println!("Aborted");
+                process::exit(0);
+            }
             for item in output.selected_items {
                 selected_item = item.output().to_string();
             }
@@ -137,14 +145,16 @@ fn main() {
             .iter()
             .find(|s| s.name == selection)
             .expect("SSH session not found in config");
-        if session_config.protocol != "ssh" {
-            println!(
-                "Invalid protocol '{}' for session '{}'",
-                session_config.protocol, session_config.name
-            );
-            return;
+        match session_config.protocol.as_str() {
+            "local" => run_local_config_session(session_config),
+            "ssh" => run_ssh_tmux_session(session_config),
+            _ => {
+                println!(
+                    "Invalid protocol '{}' for session '{}'",
+                    session_config.protocol, session_config.name
+                );
+            }
         }
-        run_ssh_tmux_session(session_config);
     }
 }
 
@@ -194,11 +204,84 @@ fn run_local_tmux_session(session_name: &str, directory: &str) {
     }
 }
 
-/// Launches a tmux session for an SSH session.
-fn run_ssh_tmux_session(session_config: &SSHSession) {
+// --- New helper function for local sessions defined in the config ---
+fn run_local_config_session(session_config: &Session) {
     let session_name = &session_config.name;
-    let host = &session_config.host;
-    let remote_command = &session_config.remote_command;
+    let command = &session_config.command; // In this case, this is the local command to run.
+    let in_tmux = env::var("TMUX").is_ok();
+    let tmux_running = Command::new("pgrep")
+        .arg("tmux")
+        .output()
+        .map(|o| !o.stdout.is_empty())
+        .unwrap_or(false);
+
+    if !in_tmux && !tmux_running {
+        Command::new("tmux")
+            .args(&["new-session", "-s", session_name])
+            .status()
+            .expect("Failed to create tmux session");
+        Command::new("tmux")
+            .args(&[
+                "send-keys",
+                "-t",
+                &format!("{}:0", session_name),
+                command,
+                "C-m",
+            ])
+            .status()
+            .expect("Failed to send keys to tmux session");
+        Command::new("tmux")
+            .args(&["attach", "-t", session_name])
+            .status()
+            .expect("Failed to attach to tmux session");
+        return;
+    }
+
+    // Create session if it doesn't exist.
+    let session_exists = Command::new("tmux")
+        .args(&["has-session", "-t", session_name])
+        .status()
+        .map(|s| s.success())
+        .unwrap_or(false);
+
+    if !session_exists {
+        Command::new("tmux")
+            .args(&["new-session", "-ds", session_name])
+            .status()
+            .expect("Failed to create detached tmux session");
+        Command::new("tmux")
+            .args(&[
+                "send-keys",
+                "-t",
+                &format!("{}:0", session_name),
+                command,
+                "C-m",
+            ])
+            .status()
+            .expect("Failed to send keys to tmux session");
+    }
+
+    // Attach or switch to the session.
+    if !in_tmux {
+        Command::new("tmux")
+            .args(&["attach", "-t", session_name])
+            .status()
+            .expect("Failed to attach to tmux session");
+    } else {
+        Command::new("tmux")
+            .args(&["switch-client", "-t", session_name])
+            .status()
+            .expect("Failed to switch tmux client");
+    }
+}
+/// Launches a tmux session for an SSH session.
+fn run_ssh_tmux_session(session_config: &Session) {
+    let session_name = &session_config.name;
+    let host = session_config
+        .host
+        .as_ref()
+        .expect("No host specified for SSH session");
+    let command = &session_config.command;
     let in_tmux = env::var("TMUX").is_ok();
     let tmux_running = Command::new("pgrep")
         .arg("tmux")
@@ -212,7 +295,7 @@ fn run_ssh_tmux_session(session_config: &SSHSession) {
             .args(&["new-session", "-s", session_name, "-d", "-n", "main"])
             .status()
             .expect("Failed to create tmux session");
-        let ssh_cmd = format!("ssh -t {} '{}; exec $SHELL'", host, remote_command);
+        let ssh_cmd = format!("ssh -t {} \"{};\"", host, command);
         Command::new("tmux")
             .args(&[
                 "send-keys",
@@ -244,7 +327,8 @@ fn run_ssh_tmux_session(session_config: &SSHSession) {
             .args(&["new-session", "-ds", session_name, "-n", "main"])
             .status()
             .expect("Failed to create detached tmux session");
-        let ssh_cmd = format!("ssh -t {} '{}; exec $SHELL'", host, remote_command);
+        let command_escape = command.replace("'", r"\'");
+        let ssh_cmd = format!("ssh -t {} \"{};\"", host, command_escape);
         Command::new("tmux")
             .args(&[
                 "send-keys",
@@ -323,7 +407,8 @@ fn create_tmux_split(session_name: &str, host: &str, split: &SplitConfig) {
     let new_pane = panes.last().expect("No pane found").to_string();
 
     // Send the SSH command to the new pane.
-    let ssh_cmd = format!("ssh -t {} '{}; exec $SHELL'", host, split.command);
+    let split_command_escape = split.command.replace("'", r"\'");
+    let ssh_cmd = format!("ssh -t {} \"{};\"", host, split_command_escape);
     Command::new("tmux")
         .args(&["send-keys", "-t", &new_pane, &ssh_cmd, "C-m"])
         .status()
